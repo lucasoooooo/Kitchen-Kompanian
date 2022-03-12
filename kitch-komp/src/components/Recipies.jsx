36e3ce089436a70025d89276f8c9371c815fd6e8
Brenton Haliw <bjhaliw@umd.edu>
2022-03-11 19:41:07 -0500

    Experimenting with MUI tables

--- kitch-komp/src/components/Recipies.jsx
+++ kitch-komp/src/components/Recipies.jsx
@@ -1,16 +1,138 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 
-function Recipies() {
-  return (
-    <div className="recipies">
-      <div className="container">
-          <h1 className="font-weight-light">Recipies</h1>
-          <p>
-            This is our Recipies List
-          </p>
-      </div>
-    </div>
-  );
+import TableComponent from "./Table"
+
+// MUI Component Imports
+import { Autocomplete, Grid, TextField, Typography } from '@mui/material'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+// MUI Icon Imports
+import Search from '@mui/icons-material/Search'
+import Filter from '@mui/icons-material/Filter'
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, prepTime, cookTime, tags) {
+  return { name, prepTime, cookTime, tags };
 }
 
-export default Recipies;
+const rows = [
+  createData('Frozen yoghurt', "30 Minutes", "2 Hours", ["Desert", "Appetizer"] ),
+  createData('Ice cream sandwich',"5 Minutes", "5 seconds", ["Desert", "Appetizer"] ),
+  createData('Eclair', "50 Minutes", "1 Hours", ["Desert", "Appetizer"]),
+  createData('Cupcake',"15 Minutes", "20 Minutes", ["Desert", "Appetizer"]),
+  createData('Gingerbread', "3 Minutes", "2 Days", ["Desert", "Appetizer"]),
+];
+
+function Recipies () {
+  const [searchText, setSearchText] = useState('')
+  const [filterTag, setFilterTag] = useState('')
+
+  const tagOptions = ['Desert', 'Dinner', 'Vegan', 'Breakfast', 'Lunch']
+
+
+
+  return (
+    <Grid
+      container
+      direction='column'
+      alignContent='center'
+      justifyContent='center'
+      alignItems='center'
+      spacing={5}
+    >
+      <Grid item>
+        <Typography compontent='center' variant='h3'>
+          Recipe List
+        </Typography>
+      </Grid>
+
+      {/* Search and Filter Options */}
+      <Grid
+        container
+        item
+        alignContent='center'
+        justifyContent='center'
+        alignItems='center'
+        spacing={3}
+      >
+        <Grid item>
+          <Search />
+        </Grid>
+
+        <Grid item>
+          <TextField label='Recipe Name' />
+        </Grid>
+
+        <Grid item>
+          <Filter />
+        </Grid>
+
+        <Grid item>
+          <Autocomplete
+            disablePortal
+            options={tagOptions}
+            renderInput={params => <TextField {...params} label='Recipe Tag' />}
+            sx={{ width: 200 }}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid item>
+        <TableComponent />
+
+      {/* <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Recipe Name</StyledTableCell>
+            <StyledTableCell align="right">Prep Time</StyledTableCell>
+            <StyledTableCell align="right">Cook Time</StyledTableCell>
+            <StyledTableCell align="right">Recipe Tags</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.prepTime}</StyledTableCell>
+              <StyledTableCell align="right">{row.cookTime}</StyledTableCell>
+              <StyledTableCell align="right">{row.tags}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer> */}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Recipies
---
+++ kitch-komp/src/components/Table.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import TextField from '@mui/material/TextField'
+import { DataGrid } from '@mui/x-data-grid'
+import ClearIcon from '@mui/icons-material/Clear'
+import SearchIcon from '@mui/icons-material/Search'
+
+function escapeRegExp (value) {
+  return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+}
+
+function QuickSearchToolbar (props) {
+  return (
+    <Box
+      sx={{
+        p: 0.5,
+        pb: 0
+      }}
+    >
+      <TextField
+        variant='standard'
+        value={props.value}
+        onChange={props.onChange}
+        placeholder='Search…'
+        InputProps={{
+          startAdornment: <SearchIcon fontSize='small' />,
+          endAdornment: (
+            <IconButton
+              title='Clear'
+              aria-label='Clear'
+              size='small'
+              style={{ visibility: props.value ? 'visible' : 'hidden' }}
+              onClick={props.clearSearch}
+            >
+              <ClearIcon fontSize='small' />
+            </IconButton>
+          )
+        }}
+        sx={{
+          width: {
+            xs: 1,
+            sm: 'auto'
+          },
+          m: theme => theme.spacing(1, 0.5, 1.5),
+          '& .MuiSvgIcon-root': {
+            mr: 0.5
+          },
+          '& .MuiInput-underline:before': {
+            borderBottom: 1,
+            borderColor: 'divider'
+          }
+        }}
+      />
+    </Box>
+  )
+}
+
+QuickSearchToolbar.propTypes = {
+  clearSearch: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
+}
+
+export default function QuickFilteringGrid () {
+  const [searchText, setSearchText] = React.useState('')
+
+  const [rows, setRows] = React.useState([
+    {
+      id: 1,
+      name: 'Banana Pudding',
+      prepTime: '30 Minutes',
+      cookTime: '10 Minutes',
+      tags: ['Dessert']
+    },
+
+    {
+      id: 2,
+      name: 'Beef Wellington',
+      prepTime: '1 Hour',
+      cookTime: '45 Miniutes',
+      tags: ['Dinner']
+    },
+
+    {
+        id: 3,
+        name: 'Baked Salmon',
+        prepTime: '5 Minutes',
+        cookTime: '10 Minutes',
+        tags: ['Dinner']
+      }
+  ])
+
+  const [columns, setColumns] = useState([
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'prepTime', headerName: 'Prep Time', width: 150 },
+    { field: 'cookTime', headerName: 'Cook Time', width: 150 },
+    { field: 'tags', headerName: 'Recipe Tags', width: 150 }
+  ])
+
+  const requestSearch = searchValue => {
+    setSearchText(searchValue)
+    const searchRegex = new RegExp(escapeRegExp(searchValue), 'i')
+
+    const filteredRows = rows.filter(row => {
+      return Object.keys(row).some(field => {
+        return searchRegex.test(row[field].toString())
+      })
+    })
+    setRows(filteredRows)
+  }
+
+  React.useEffect(() => {
+    setRows(rows)
+  }, [rows])
+
+  return (
+    <Box sx={{ height: 400, width: 1 }}>
+      <DataGrid
+        components={{ Toolbar: QuickSearchToolbar }}
+        rows={rows}
+        columns={columns}
+        componentsProps={{
+          toolbar: {
+            value: searchText,
+            onChange: event => requestSearch(event.target.value),
+            clearSearch: () => requestSearch('')
+          }
+        }}
+      />
+    </Box>
+  )
+}
