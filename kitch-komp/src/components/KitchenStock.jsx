3e395d9054ac0faee3c9fdf98b1941fc6f2bd947
Lucas Balangero <lbalange@terpmail.umd.edu>
2022-03-18 16:57:41 -0400

    Added styling and deleted unused elements

--- kitch-komp/src/components/KitchenStock.jsx
+++ kitch-komp/src/components/KitchenStock.jsx
@@ -1,108 +1,106 @@
 import React from "react";
+import Box from "@mui/material/Box"
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-// import { Input } from "@mui/material";
 import { DataGrid } from '@mui/x-data-grid';
 
 function KitchenStock() {
   // These food items will later come from useState's
   // TODO: Remove these
   const refrig_things = 
-  [{name:"Bread", qty:"1 Loaf", id:1}, {name:"Mayo", qty:"-", id:2}, {name:"Ham",qty:"-", id:3}]
-  const freezer_things = [{name:"Icecream", qty:"-"}, {name:"Peas", qty:"1 bag"}]
-  const pantry_things = [{name:"Captain Crunch", qty:"-"}, {name:"Peanut Butter", qty:"-"}]
-  const [edit, setEdit] = React.useState(false)
-  const [add, setAdd] = React.useState(false)
-  const refrig_element = refrig_things.map(function(p){
-      return (
-          <nav className="food-item">
-              <h3>{p.name}</h3>
-              <h3>{p.qty}</h3>
-              <MoreHorizIcon className="edit" onClick={flipEdit}/>
-          </nav>)
-  })
-  const freezer_element = freezer_things.map(function(p){
-      return (
-          <nav className="food-item">
-              <h3>{p.name}</h3>
-              <h3>{p.qty}</h3>
-              <MoreHorizIcon className="edit" onClick={flipEdit}/>
-          </nav>)
-  })
-  const pantry_element = pantry_things.map(function(p){
-      return (
-      <nav className="food-item">
-          <h3>{p.name}</h3>
-          <h3>{p.qty}</h3>
-          <MoreHorizIcon className="edit" onClick={flipEdit}/>
-      </nav>)
-  })
-  function flipEdit(){
-    setEdit(prev => !prev)
-  }
-  function flipAdd(){
-    setAdd(prev => !prev)
-  }
-  const columns = [{field:"name", headerName: "name", width:150},{field:"qty", headerName:"qty", width:150}];
+    [{name:"Bread", qty:"1 Loaf", id:1}, {name:"Mayo", qty:"-", id:2}, {name:"Ham",qty:"-", id:3}]
+  const freezer_things = 
+    [{name:"Icecream", qty:"-", id:1}, {name:"Peas", qty:"1 bag", id:2}]
+  const pantry_things = 
+    [{name:"Captain Crunch", qty:"-", id:1}, {name:"Peanut Butter", qty:"-",id:2}]
+  
+  const columns = [{field:"name", headerName: "Name", width:300},{field:"qty", headerName:"Quantity", width:300}];
   return (
     <div className="kitchenStock">
       
-      {!edit && !add && <div className="list">
+      <div className="list">
           <h1 className="font-weight-light">Kitchen Stock List</h1>
         <nav className="kitchen-stock-headers">
             <h2>Refrigerator</h2>
-            
-            <AddCircleOutlineIcon className="add-sign" onClick={flipAdd}/>
+            <AddCircleOutlineIcon className="add-sign" />
         </nav>
+        <Box sx={{
+          height:300,
+          width: 1, 
+          '& .MuiDataGrid-columnHeaders':{
+          backgroundColor: '#6A994E'},
+          '& .even':{
+            backgroundColor: 'rgb(223, 212, 184)'
+          },
+          '& .odd':{
+            backgroundColor: 'rgb(244, 233, 204)'
+          }
+          
+          }}>
         <DataGrid
-              // components = {{Toolbar: QuickSearchToolBar}}
-              // getRowId={(r) => r.DT_RowId}
               columns={columns}
               rows={refrig_things}
-              // rowsHeight={10}
-              // pageSize={100}
-              // filterMode="server"
-              // ItemsSource="{Binding Clients.RowItems}"
-              // // onFilterModelChange={onFilterChange}
-              // // loading={loading}
+              getCellClassName={(params)=>{
+                return params.id%2 ===1 ? 'even' : 'odd'
+              }}
+              
         />
-        <ul>
-            {/* <li>Name     QTY</li> */}
-            {refrig_element}
-        </ul>
+        </Box>
+
         <nav className="kitchen-stock-headers">
             <h2>Freezer</h2>
-            <AddCircleOutlineIcon className="add-sign" onClick={flipAdd}/>
+            <AddCircleOutlineIcon className="add-sign"/>
         </nav>
-        <ul>{freezer_element}</ul>
+        <Box sx={{
+          height:300,
+          width: 1, 
+          '& .MuiDataGrid-columnHeaders':{
+          backgroundColor: '#6A994E'},
+          '& .even':{
+            backgroundColor: 'rgb(223, 212, 184)'
+          },
+          '& .odd':{
+            backgroundColor: 'rgb(244, 233, 204)'
+          }
+          
+          }}>
+        <DataGrid
+              columns={columns}
+              rows={freezer_things}
+              getCellClassName={(params)=>{
+                return params.id%2 ===1 ? 'even' : 'odd'
+              }}
+        />
+        </Box>
+
         <nav className="kitchen-stock-headers">
             <h2>Pantry</h2>
-            <AddCircleOutlineIcon className="add-sign" onClick={flipAdd}/>
+            <AddCircleOutlineIcon className="add-sign"/>
         </nav>
-        <ul>{pantry_element}</ul>
-      </div>}
-      {edit && !add && <form className="editing">
-        <label for="name">Change Name: </label>
-        <input type="text" id="name" name="name"></input><br/>
-        <label for="qty">Change Quantity: </label>
-        <input type="text" id="qty" name="qty"></input><br/>
-        <nav className="delete-apply">
-          <button onClick={flipEdit}>Delete</button>
-          <input type="submit" value="Apply"></input>
-        </nav>
-        <br/><br/>
-      </form>}
-      {add && !edit && <form className="editing">
-        <label for="name">Name: </label>
-        <input type="text" id="name" name="name"></input><br/>
-        <label for="qty">Quantity: </label>
-        <input type="text" id="qty" name="qty"></input><br/>
-        <nav className="delete-apply">
-          <button onClick={flipAdd}>Delete</button>
-          <input type="submit" value="Add"></input>
-        </nav>
-        <br/><br/>
-      </form>}
+        <Box sx={{
+          height:300,
+          width: 1, 
+          '& .MuiDataGrid-columnHeaders':{
+          backgroundColor: '#6A994E'},
+          '& .even':{
+            backgroundColor: 'rgb(223, 212, 184)'
+          },
+          '& .odd':{
+            backgroundColor: 'rgb(244, 233, 204)'
+          }
+          
+          }}>
+
+          <DataGrid 
+                columns={columns}
+                rows={pantry_things}
+                getCellClassName={(params)=>{
+                  return params.id%2 ===1 ? 'even' : 'odd'
+                }}
+          />
+        </Box>
+      </div>
+      
+      <br/><br/>
 
     </div>
   );
